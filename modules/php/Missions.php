03175d1346560ff93d91570c09cbab2219c4a70f
Michał Delikat <michal.delikat0@gmail.com>
2026-07-03 16:10:06 +0200

    Mission configuration refactor

--- modules/php/Missions.php
+++ modules/php/Missions.php
@@ -5,103 +5,82 @@ trait Missions {
     protected function configureMissions(string $missionAName, string $missionBName): void {
         $this->setSelectedMissions($missionAName, $missionBName);
 
+        $missionNames = [$missionAName, $missionBName];
+
         $missionsWithSpaces = [
-            MISSION_OFFICERS_MANSION, 
-            MISSION_SABOTAGE, 
-            MISSION_INFILTRATION, 
-            MISSION_GERMAN_SHEPARDS, 
-            MISSION_UNDERGROUND_NEWSPAPER,
-            MISSION_AID_THE_SPY,
-            MISSION_DESTROY_THE_TRAIN,
-            MISSION_CODED_MESSAGES,
-            MISSION_BOMB_FOR_THE_OFFICER,
-            MISSION_BOMB_THE_BARRACKS,
-            MISSION_FREE_THE_RESISTANCE_LEADER,
-            MISSION_DESTROY_AA_GUNS
+            MISSION_OFFICERS_MANSION, MISSION_SABOTAGE, MISSION_INFILTRATION,
+            MISSION_GERMAN_SHEPARDS, MISSION_UNDERGROUND_NEWSPAPER, MISSION_AID_THE_SPY,
+            MISSION_DESTROY_THE_TRAIN, MISSION_CODED_MESSAGES, MISSION_BOMB_FOR_THE_OFFICER,
+            MISSION_BOMB_THE_BARRACKS, MISSION_FREE_THE_RESISTANCE_LEADER, MISSION_DESTROY_AA_GUNS,
         ];
 
         if (in_array($missionAName, $missionsWithSpaces)) {
             $this->addMissionSpace(MISSION_A_SPACE_A);
         }
-
         if (in_array($missionBName, $missionsWithSpaces)) {
             $this->addMissionSpace(MISSION_B_SPACE_A);
         }
 
-        $missionNames = [$missionAName, $missionBName];
+        $missionSpaceActions = [
+            MISSION_OFFICERS_MANSION      => ACTION_COMPLETE_OFFICERS_MANSION_MISSION,
+            MISSION_SABOTAGE              => ACTION_INFILTRATE_FACTORY,
+            MISSION_UNDERGROUND_NEWSPAPER => ACTION_DELIVER_2_INTEL,
+            MISSION_INFILTRATION          => ACTION_INSERT_MOLE,
+            MISSION_GERMAN_SHEPARDS       => ACTION_POISON_SHEPARDS,
+            MISSION_AID_THE_SPY           => ACTION_DELIVER_2_WEAPONS,
+            MISSION_DESTROY_THE_TRAIN     => ACTION_DELIVER_3_EXPLOSIVES,
+            MISSION_CODED_MESSAGES        => ACTION_TRAIN_A_CRYPTOGRAPHER,
+            MISSION_BOMB_FOR_THE_OFFICER  => ACTION_DELIVER_EXPLOSIVES_AND_WEAPON,
+        ];
+
+        foreach ($missionSpaceActions as $mission => $action) {
+            if ($mission === $missionAName) {
+                $this->addSpaceAction(MISSION_A_SPACE_A, $action);
+            } else if ($mission === $missionBName) {
+                $this->addSpaceAction(MISSION_B_SPACE_A, $action);
+            }
+        }
 
         if (in_array(MISSION_MILICE_PARADE_DAY, $missionNames)) {
             $this->addSpaceAction(RUE_BARADAT, ACTION_COMPLETE_MILICE_PARADE_DAY_MISSION);
         }
-        
+
         if (in_array(MISSION_OFFICERS_MANSION, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_OFFICERS_MANSION ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            foreach([RUE_BARADAT, 3, PONT_LEVEQUE] as $space) { 
+            foreach ([RUE_BARADAT, PONT_DU_NORD, PONT_LEVEQUE] as $space) {
                 $this->addSpaceAction($space, ACTION_WRITE_GRAFFITI);
             }
-            $this->addSpaceAction($missionSpace, ACTION_COMPLETE_OFFICERS_MANSION_MISSION);
-        }
-
-        if (in_array(MISSION_SABOTAGE, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_SABOTAGE ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_INFILTRATE_FACTORY);
-        }
-
-        if (in_array(MISSION_UNDERGROUND_NEWSPAPER, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_UNDERGROUND_NEWSPAPER ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_DELIVER_2_INTEL);
-        }
-
-        if (in_array(MISSION_INFILTRATION, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_INFILTRATION ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_INSERT_MOLE);
-        }
-
-        if (in_array(MISSION_GERMAN_SHEPARDS, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_GERMAN_SHEPARDS ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_POISON_SHEPARDS);
-        }
-
-        if (in_array(MISSION_AID_THE_SPY, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_AID_THE_SPY ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_DELIVER_2_WEAPONS);
-        }
-
-        if (in_array(MISSION_DESTROY_THE_TRAIN, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_DESTROY_THE_TRAIN ? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_DELIVER_3_EXPLOSIVES);
-        }
-
-        if (in_array(MISSION_CODED_MESSAGES, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_CODED_MESSAGES? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_TRAIN_A_CRYPTOGRAPHER);
         }
 
         if (in_array(MISSION_TAKE_OUT_THE_BRIDGES, $missionNames)) {
             $this->addSpaceAction(BLACK_MARKET, ACTION_PLANT_2_EXPLOSIVES);
         }
 
-        if (in_array(MISSION_BOMB_FOR_THE_OFFICER, $missionNames)) {
-            $missionSpace = $missionAName === MISSION_BOMB_FOR_THE_OFFICER? MISSION_A_SPACE_A : MISSION_B_SPACE_A;
-            $this->addSpaceAction($missionSpace, ACTION_DELIVER_EXPLOSIVES_AND_WEAPON);
-        }
+        match ($missionBName) {
+            MISSION_MILICE_HQ                  => $this->setupMiliceHQ(),
+            MISSION_BOMB_THE_BARRACKS          => $this->setupBombTheBarracks(),
+            MISSION_FREE_THE_RESISTANCE_LEADER => $this->addSpaceAction(MISSION_B_SPACE_A, ACTION_BRIBE_THE_CLERK),
+            MISSION_DESTROY_AA_GUNS            => $this->setupDestroyAAGuns(),
+        };
+    }
 
-        if ($missionBName === MISSION_MILICE_HQ) {
-            $this->addSpaceAction(RUE_BARADAT, ACTION_DISCOVER_THE_PLANS);
-            $this->setMorale(4, false);
-        } else if ($missionBName === MISSION_BOMB_THE_BARRACKS) {
-            $this->addSpaceAction(MISSION_B_SPACE_A, ACTION_RECON_THE_BARRACKS);
-            $this->setActiveSoldiers(3);
-        } else if ($missionBName === MISSION_FREE_THE_RESISTANCE_LEADER) {
-            $this->addSpaceAction(MISSION_B_SPACE_A, ACTION_BRIBE_THE_CLERK);
-        } else if ($missionBName === MISSION_DESTROY_AA_GUNS) {
-            $this->placeTokens(RUE_BARADAT, 'aa_gun', 1, false);
-            $this->placeTokens(BLACK_MARKET, 'aa_gun', 1, false);
-            $this->placeTokens(LEFT_FIELD, 'aa_gun', 1, false);
-            $this->placetokens(RIGHT_FIELD, 'aa_gun', 1, false);
-            $this->addSpaceAction([RUE_BARADAT, BLACK_MARKET, LEFT_FIELD, RIGHT_FIELD, MISSION_B_SPACE_A], ACTION_DESTROY_AA_GUN_WITH_WEAPON);
-            $this->addSpaceAction([RUE_BARADAT, BLACK_MARKET, LEFT_FIELD, RIGHT_FIELD, MISSION_B_SPACE_A], ACTION_DESTROY_AA_GUN_WITH_EXPLOSIVES);
+    protected function setupMiliceHQ(): void {
+        $this->addSpaceAction(RUE_BARADAT, ACTION_DISCOVER_THE_PLANS);
+        $this->setMorale(4, false);
+    }
+
+    protected function setupBombTheBarracks(): void {
+        $this->addSpaceAction(MISSION_B_SPACE_A, ACTION_RECON_THE_BARRACKS);
+        $this->setActiveSoldiers(3);
+    }
+
+    protected function setupDestroyAAGuns(): void {
+        $outerSpaces = [RUE_BARADAT, BLACK_MARKET, LEFT_FIELD, RIGHT_FIELD];
+        foreach ($outerSpaces as $space) {
+            $this->placeTokens($space, 'aa_gun', 1, false);
         }
+        $allSpaces = [...$outerSpaces, MISSION_B_SPACE_A];
+        $this->addSpaceAction($allSpaces, ACTION_DESTROY_AA_GUN_WITH_WEAPON);
+        $this->addSpaceAction($allSpaces, ACTION_DESTROY_AA_GUN_WITH_EXPLOSIVES);
     }
 
     protected function addMissionSpace(int $spaceID): void {
